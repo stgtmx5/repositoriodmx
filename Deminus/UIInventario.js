4ee33e5f42243feaf369e1f976f329d37a05343e
georgemx5 <jmartinez@stgt.com.mx>
2025-07-07 15:24:56 -0600

    JM:Actualizacion Traspasos 2

--- Deminus/UIInventario.js
+++ Deminus/UIInventario.js
@@ -892,6 +892,7 @@ function Traspaso(A){
 			}
 		}
 	try{
+		// JM:Registro del usuario que realiza el traspaso 
 		sql = "INSERT INTO stgt_log_auditoria (operacion, tabla, fecha, usuario, log)  VALUES ('TRASPASOS','Cardex',now()," + Application.UIUsers.CurrentUser.Sys_PK + ",'INSERCION DE TRASPASO " + Ref +" ')";
 		Application.ADOCnn.Execute(sql);
 		
